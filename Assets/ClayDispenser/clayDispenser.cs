a48e9eec9ac41ba208e4e007b857bc32ff1484a3
TheCell <simon_hischier@hotmail.com>
2018-08-05 14:04:11 +0200

    added claydispenser

---
+++ Assets/ClayDispenser/clayDispenser.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clayDispenser : MonoBehaviour
+{
+    public int shootCycleCount = 5;
+    public float secondsBetweenShots = 2.0f;
+    public GameObject shootFrom;
+    public GameObject shootObject;
+
+    private int shootCounter;
+    private float minForce = 1300.0f;
+    private float maxForce = 2000.0f;
+    private float lastDispenseTime;
+
+    // Use this for initialization
+    void Start ()
+    {
+        lastDispenseTime = Time.realtimeSinceStartup;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        shootInterval();
+    }
+
+    public void startDispenser()
+    {
+        shootCounter = shootCycleCount;
+    }
+
+    private void dispense()
+    {
+        updateDispenseTime();
+
+        if (objectreferencesSet())
+        {
+            GameObject shootdisc = Instantiate(shootObject);
+            shootdisc.transform.position = shootFrom.transform.position;
+            applyForce(shootdisc);
+        }
+        else
+        {
+            print("missing Object References");
+        }
+    }
+
+    private bool objectreferencesSet()
+    {
+        return (shootFrom != null && shootObject != null);
+    }
+
+    private void applyForce(GameObject objectToApplyTo)
+    {
+        Rigidbody rb = objectToApplyTo.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            Vector3 dispenseVector = shootFrom.transform.forward;
+            dispenseVector.x += Random.Range(-1.0f, 1.0f);
+            dispenseVector.z += Random.Range(-1.0f, 1.0f);
+            rb.AddForce(dispenseVector * Mathf.Lerp(minForce, maxForce, Random.Range(0.0f, 1.0f)));
+        }
+    }
+
+    private void shootInterval()
+    {
+        if(this.shootCounter > 0 && cooldownExpired())
+        {
+            shootCounter--;
+            dispense();
+        }
+    }
+
+    private void updateDispenseTime()
+    {
+        lastDispenseTime = Time.realtimeSinceStartup;
+    }
+
+    private bool cooldownExpired()
+    {
+        return (Time.realtimeSinceStartup - this.lastDispenseTime > this.secondsBetweenShots);
+    }
+}
---
+++ Assets/ClayDispenser/startButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startButton : MonoBehaviour
+{
+    public clayDispenser clayDispenser;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        print("collision");
+        if (clayDispenser != null)
+        {
+            clayDispenser.startDispenser();
+        }
+        else
+        {
+            print("no script reference");
+        }
+    }
+}
