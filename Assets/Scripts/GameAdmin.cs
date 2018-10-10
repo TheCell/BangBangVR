05dded0be35b7cfc82918da15a47ef40dbb73b11
TheCell <simon_hischier@hotmail.com>
2018-10-10 19:58:35 +0200

    overview class

---
+++ Assets/Scripts/GameAdmin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAdmin : MonoBehaviour
+{
+	private Highscore highscore;
+
+	// Use this for initialization
+	void Start ()
+	{
+		highscore = new Highscore();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
