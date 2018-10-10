fb5e8932e776385f8329e90dec26f6e3aade44d2
TheCell <simon_hischier@hotmail.com>
2018-10-10 19:29:17 +0200

    added highscore class

---
+++ Assets/Scripts/Highscore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highscore : MonoBehaviour
+{
+	private int highScore = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	public void resetHighScore()
+	{
+		highScore = 0;
+	}
+
+	public int getHighScore()
+	{
+		return highScore;
+	}
+
+	public void addToHighscore(int scoreToAdd)
+	{
+		this.highScore += scoreToAdd;
+	}
+}
